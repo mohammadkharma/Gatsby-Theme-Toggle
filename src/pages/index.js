eef33511794867e52105230293d9f79172dc2503
MYK <mohammad.kh.8688@gmail.com>
2021-04-15 16:37:54 +0200

    added main app code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
-import React from "react"
+import React, { useContext } from "react"
+import { css } from "@emotion/core"
+import { useTheme } from "@emotion/react"
+import Context from "../store/context"
+import Layout from "../components/Layout"
 
-export default function Home() {
-  return <div>Theme Toggle</div>
+const IndexPage = () => {
+  const { state, dispatch } = useContext(Context)
+
+  const theme = useTheme()
+
+  return (
+    <Layout>
+      <div
+        css={css`
+          h1,
+          h2 {
+            color: ${state.isDark ? theme.dark.font : theme.light.font};
+          }
+        `}
+      >
+        <h1>Hello World</h1>
+        <button onClick={() => dispatch({ type: "TOGGLE_DARK_MODE" })}>
+          Toggle Dark Mode
+        </button>
+        <h2>Dark Mode is {state.isDark ? "on" : "off"}</h2>
+      </div>
+    </Layout>
+  )
 }
