ff64b884c9af6c339b0119e27605b3b6422ab0a3
MYK <mohammad.kh.8688@gmail.com>
2021-04-15 14:16:19 +0200

    created custom react hook for global state

---
+++ src/store/useGlobalState.js
@@ -0,0 +1,21 @@
+import { useReducer } from "react"
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_DARK_MODE":
+      return {}
+    default: {
+      return state
+    }
+  }
+}
+
+const useGlobalState = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    isDark: false,
+  })
+
+  return { state, dispatch }
+}
+
+export default useGlobalState
