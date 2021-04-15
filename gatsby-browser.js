e19af0031a6f9fceca990636a0e45d6fe062d8aa
MYK <mohammad.kh.8688@gmail.com>
2021-04-15 14:51:28 +0200

    wrapped the app with ThemeProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,10 @@
 import React from "react"
+import { ThemeProvider } from "emotion-theming"
 import GlobalStateProvider from "./src/store/GlobalStateProvider"
+import { theme } from "./src/theme/theme"
 
 export const wrapRootElement = ({ element }) => (
-  <GlobalStateProvider>{element}</GlobalStateProvider>
+  <GlobalStateProvider>
+    <ThemeProvider theme={theme}>{element}</ThemeProvider>
+  </GlobalStateProvider>
 )
